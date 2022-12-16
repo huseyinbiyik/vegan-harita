3f02453daf08f2b16a13d24ee8f563126c123c80
Huseyin Biyik <tahahuseyinb@gmail.com>
2022-12-17 00:49:45 +0300

    Add Google maps to the homepage

---
+++ app/javascript/controllers/map_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="map"
+export default class extends Controller {
+  connect() {
+    // Initialize and add the map
+    function initMap() {
+      // The location of Uluru
+      const uluru = { lat: -25.344, lng: 131.031 };
+      // The map, centered at Uluru
+      const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: uluru,
+      });
+      // The marker, positioned at Uluru
+      const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+      });
+    }
+
+    window.initMap = initMap;
+  }
+}
