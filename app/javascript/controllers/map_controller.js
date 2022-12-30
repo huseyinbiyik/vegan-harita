86c529301c6bdf3c87d08f815d03071872803af1
Huseyin Biyik <25887253+huseyinbiyik@users.noreply.github.com>
2022-12-30 12:15:46 +0300

    Add Google Maps user current location functionality

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -21,22 +21,55 @@ export default class extends Controller {
         zoom: 14,
         center: kadikoy,
       });
-      // The marker, positioned at Kadikoy
-      //   const marker = new google.maps.Marker({
-      //     position: kadikoy,
-      //     map: map,
-      //   });
-      // };
 
       const infoWindow = new google.maps.InfoWindow({
         content: "",
         disableAutoPan: true,
       });
-      // Create an array of alphabetical characters used to label the markers.
-      const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+      const locationButton = document.createElement("button");
+      locationButton.textContent = "Pan to Current Location";
+      locationButton.classList.add("custom-map-control-button");
+      map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+
+      locationButton.addEventListener("click", () => {
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              const pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+              };
+
+              infoWindow.setPosition(pos);
+              infoWindow.setContent("Location found.");
+              infoWindow.open(map);
+              map.setCenter(pos);
+            },
+            () => {
+              handleLocationError(true, infoWindow, map.getCenter());
+            }
+          );
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      });
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(
+          browserHasGeolocation
+            ? "Error: The Geolocation service failed."
+            : "Error: Your browser doesn't support geolocation."
+        );
+        infoWindow.open(map);
+      }
+
       // Add some markers to the map.
-      const markers = locations.map((position, i) => {
-        const label = labels[i % labels.length];
+      const markers = locations.map((position) => {
+        let label = position.name;
         const marker = new google.maps.Marker({
           position,
           label,
@@ -56,7 +89,6 @@ export default class extends Controller {
     };
 
     const locations = JSON.parse(this.mapTarget.dataset.mapPlaces);
-
 
     // Append the 'script' element to 'head'
     document.head.appendChild(script);
