1aa42c3d3e5d0d6d5dc1ffd85e27cd4cbc45639d
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2023-03-06 17:39:19 +0300

    Rename stimulus controller from nav to search

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
-    console.log("Navigation controller connected");
+    console.log("Search controller connected");
   }
 
   toggle() {
