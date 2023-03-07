9ada5d39da4121c69d406ff1ebd65d805b74e2db
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2023-03-07 15:46:19 +0300

    Move js icons to public folder

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -30,7 +30,7 @@ export default class extends Controller {
 
         // Set CSS for the control interior
         var controlText = document.createElement("img");
-        controlText.src = "assets/zoom-in.svg";
+        controlText.src = "zoom-in.svg";
         controlUI.appendChild(controlText);
 
         // Setup the click event listeners
@@ -44,7 +44,7 @@ export default class extends Controller {
 
         // Set CSS for the control interior
         var controlTextLeft = document.createElement("img");
-        controlTextLeft.src = "assets/zoom-out.svg";
+        controlTextLeft.src = "zoom-out.svg";
         controlUILeft.appendChild(controlTextLeft);
 
         // Setup the click event listeners
@@ -113,7 +113,7 @@ export default class extends Controller {
       }
 
       const locationButton = document.createElement("button");
-      locationButton.innerHTML = `<img src="${window.location.origin}/assets/find-me.svg" alt="find-me">`;
+      locationButton.innerHTML = `<img src="find-me.svg" alt="find-me">`;
       locationButton.classList.add("custom-map-control-button");
       locationButton.id = "location-button";
       map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(
@@ -123,8 +123,8 @@ export default class extends Controller {
       centerToMyCurrentLocation();
 
       // Marker icons located on public folder
-      const VeganMarkerIcon = "assets/vegan-place.svg";
-      const veganFriendlyMarkerIcon = "assets/vegan-friendly-place.svg";
+      const VeganMarkerIcon = "vegan-place.svg";
+      const veganFriendlyMarkerIcon = "vegan-friendly-place.svg";
 
       console.log(locations);
       // Create markers for each location
