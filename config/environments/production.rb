85f9468f2112bcbd178088df1110a647fefb1711
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2023-04-02 00:41:44 +0300

    Add default url option for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Mailer Configuration
+  config.action_mailer.default_url_options = { host: 'veganharita.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
