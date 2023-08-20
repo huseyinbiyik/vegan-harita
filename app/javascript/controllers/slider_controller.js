9e9f95c8b6f376f6cc9bbf3eb89bfde65873a0d0
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2023-08-20 15:26:11 +0300

    Add slider number indicator

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -1,16 +1,17 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["image"];
+  static targets = ["image", "number"];
 
   initialize() {
     this.index = 0;
     this.showCurrentImage();
+    console.log(this.imageTargets.length);
   }
 
   next() {
     if (this.index === this.imageTargets.length - 1) {
-      this.index = 0;
+      this.index = -1;
     }
     this.index++;
     this.showCurrentImage();
@@ -18,15 +19,22 @@ export default class extends Controller {
 
   previous() {
     if (this.index === 0) {
-      this.index = this.imageTargets.length - 1;
+      this.index = this.imageTargets.length;
     }
     this.index--;
     this.showCurrentImage();
   }
 
   showCurrentImage() {
+    this.updateSliderNumber();
     this.imageTargets.forEach((element, index) => {
       element.hidden = index !== this.index;
     });
+  }
+
+  updateSliderNumber() {
+    this.numberTarget.innerText = `${this.index + 1} / ${
+      this.imageTargets.length
+    }`;
   }
 }
