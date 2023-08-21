0a0b7a12846950cf194fd5bfe5a7e7a25a70e5c6
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2023-08-21 23:07:16 +0300

    Add recaptcha to signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :places do
     resources :menus
     collection do
