5471e505efd287d188f9d39d31f12cb9093704ab
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2023-10-23 13:40:23 +0300

    Add likes polymorphic model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_19_200156) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_23_103845) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_19_200156) do
     t.bigint "user_id", null: false
     t.index ["changeable_type", "changeable_id"], name: "index_change_logs_on_changeable"
     t.index ["user_id"], name: "index_change_logs_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["record_type", "record_id"], name: "index_likes_on_record"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "menus", force: :cascade do |t|
@@ -132,6 +142,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_19_200156) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "change_logs", "users"
+  add_foreign_key "likes", "users"
   add_foreign_key "menus", "places"
   add_foreign_key "reviews", "places"
   add_foreign_key "reviews", "users"
