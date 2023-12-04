b39132e2e980eeef76c96e44489830d98ffb5194
Bengi Genç <genc.bengi@gmail.com>
2023-12-04 21:59:38 +0300

    Set English default locale language

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module VhApi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :en
   end
 end
