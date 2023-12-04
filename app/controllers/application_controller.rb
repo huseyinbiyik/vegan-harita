947c257c516e3ddeec65ef3f513a56cbd17c6cf0
Bengi Genç <genc.bengi@gmail.com>
2023-12-04 21:57:04 +0300

    Set locale based on browser settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = extract_locale_from_accept_language_header
+  end
+
+  def extract_locale_from_accept_language_header
+    accept_language = request.env['HTTP_ACCEPT_LANGUAGE']
+    accept_language&.scan(/^[a-z]{2}/)&.first
+  end
 
   protected
 
