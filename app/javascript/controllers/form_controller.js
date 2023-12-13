5e3ea6b8a03c9e6fad8104cfa22f0f6d94f417ae
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2023-12-13 22:19:01 +0300

    Add close form after submit place edit form

---
+++ app/javascript/controllers/form_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="form"
+export default class extends Controller {
+  static targets = ["frame"];
+
+  hideFrame() {
+    this.frameTarget.remove();
+  }
+
+  submitEnd(e) {
+    if (e.detail.success) {
+      this.hideFrame();
+    }
+  }
+}
