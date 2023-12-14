53e36d965b17c4ff44cc07eaf6bca0b781f3c01d
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2023-12-14 20:48:31 +0300

    Update reviews controller responses

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_review, only: %i[edit update destroy]
-  before_action :set_place, only: %i[new edit create update]
+  before_action :set_place, only: %i[new edit create update destroy]
 
   def new
     @review = Review.new
@@ -12,23 +12,39 @@ class ReviewsController < ApplicationController
   def create
     @review = @place.reviews.build(review_params)
     @review.user = current_user
-    if @review.save
-      redirect_to place_url(@place), notice: 'Değerlendirmeniz başarıyla kaydedildi.'
-    else
-      redirect_to place_url(@place), notice: 'Değerlendirmeniz kaydedilemedi.'
+    respond_to do |format|
+      if @review.save
+        format.turbo_stream do
+          flash.now[:notice] = t('controllers.reviews.create.success')
+          render turbo_stream: turbo_stream.update('flash_messages', partial: 'shared/flash_messages',
+                                                                     locals: { flash: })
+        end
+      else
+        render format.turbo_stream do
+          flash.now[:alert] = t('controllers.reviews.create.failure')
+          render turbo_stream: turbo_stream.update('flash_messages', partial: 'shared/flash_messages',
+                                                                     locals: { flash: })
+        end
+      end
     end
   end
 
   def update
     if @review.update(review_params)
-      redirect_to [@place, @review], notice: 'Review was successfully updated.'
+      flash.now[:notice] = t('controllers.reviews.update.success')
+      render turbo_stream: turbo_stream.update('flash_messages', partial: 'shared/flash_messages',
+                                                                 locals: { flash: })
     else
-      render :edit
+      redirect_to @place, alert: t('controllers.reviews.update.failure')
     end
   end
 
   def destroy
-    @review.destroy
+    if @review.destroy
+      redirect_to @place, notice: t('controllers.reviews.destroy.success')
+    else
+      redirect_to @place, alert: t('controllers.reviews.destroy.failure')
+    end
   end
 
   private
