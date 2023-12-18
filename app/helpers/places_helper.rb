32d5bf4b93ea12dd1bee0728dfd0b844ef69be7a
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2023-12-18 22:42:08 +0300

    Refactor social link methods in PlacesHelper and view

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,25 +1,14 @@
 module PlacesHelper
-  def instagram_link(place)
-    link_to('Instagram', place.instagram_handle, target: '_blank') if place.instagram_handle.present?
-  end
-
-  def facebook_link(place)
-    link_to('Facebook', place.facebook_handle, target: '_blank') if place.facebook_handle.present?
-  end
-
-  def twitter_link(place)
-    link_to('Twitter', place.x_handle, target: '_blank') if place.x_handle.present?
-  end
-
-  def web_link(place)
-    link_to('Web', place.web_url, target: '_blank') if place.web_url.present?
-  end
-
-  def email_link(place)
-    link_to('Email', "mailto:#{place.email}") if place.email.present?
-  end
-
-  def phone_link(place)
-    link_to('Phone', "tel:#{place.phone}") if place.phone.present?
+  def social_link_url(key, value)
+    case key
+    when 'instagram', 'facebook', 'x'
+      "https://www.#{key}.com/#{value}"
+    when 'web'
+      "https://#{value}"
+    when 'email'
+      "mailto:#{value}"
+    else
+      value
+    end
   end
 end
