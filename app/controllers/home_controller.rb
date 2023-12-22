da2b13880de7948ba786bc84f9d0e18884df40e2
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2023-12-23 01:56:45 +0300

    Add reCAPTCHA validation and error handling for contact form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,17 @@
 class HomeController < ApplicationController
+  prepend_before_action :check_captcha, only: [:send_mail]
   def contact; end
 
   def send_mail
     ContactMailer.contact_email(params[:name], params[:email], params[:message], params[:images]).deliver_now
-    redirect_to contact_path, notice: 'Message received, thanks!'
+    redirect_to contact_path, notice: t('controllers.home.message_received')
+  end
+
+  private
+
+  def check_captcha
+    return if verify_recaptcha
+
+    redirect_to contact_path, alert: t(:captcha_error)
   end
 end
