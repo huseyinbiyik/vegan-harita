25742555bd63cafc31faba347a7d1afb09a78720
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2023-12-23 22:53:05 +0300

    Fix linters

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get contact" do
+  test 'should get contact' do
     get home_contact_url
     assert_response :success
   end
--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class ContactMailerTest < ActionMailer::TestCase
   # test "the truth" do
