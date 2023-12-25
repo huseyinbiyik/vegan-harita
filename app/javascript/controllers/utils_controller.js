52cafe026279a903153f4eecd9a33b6a89563853
Bengi Genç <genc.bengi@gmail.com>
2023-12-25 17:28:39 +0300

    Update file upload styling and functionality for new place page

--- app/javascript/controllers/utils_controller.js
+++ app/javascript/controllers/utils_controller.js
@@ -2,12 +2,22 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="utils"
 export default class extends Controller {
-  static targets = ["preview"];
+  static targets = ["preview", "uploadBtn"];
+  static values = { assets: Array};
+
   uploadAvatar(e) {
     let reader = new FileReader();
     reader.onload = (e) => {
       this.previewTarget.src = e.target.result;
     };
     reader.readAsDataURL(e.target.files[0]);
+  }
+  uploadFiles (event) {
+    const inputElement = event.target;
+    if (inputElement.files.length > 0) {
+      this.uploadBtnTarget.textContent = `${inputElement.files.length} ${this.assetsValue[0]}`;
+    } else {
+      this.uploadBtnTarget.textContent = `${this.assetsValue[1]}`;
+    }
   }
 }
