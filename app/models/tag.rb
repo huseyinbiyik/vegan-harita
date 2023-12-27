643d8c3020224cb29093786f07d3663621805c15
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2023-12-28 00:37:53 +0300

    Update tag names in form to use translations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,12 @@
 class Tag < ApplicationRecord
   has_and_belongs_to_many :places
+  before_save :downcase_name
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 30 }
+
+  private
+
+  def downcase_name
+    name.downcase!
+  end
 end
