7b75632b0fe2d58c0768c1b0f44ec76e9e08e9b7
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2023-12-31 19:44:45 +0300

    Fix devise parameter sanitizer keys in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: %i[email password password_confirmation user_agreement_accepted username ])
+                                      keys: %i[email password password_confirmation user_agreement_accepted username])
     devise_parameter_sanitizer.permit(:account_update,
                                       keys: [ :approved, :role, :points, :avatar, :locale, :admin_note, :username ])
   end
