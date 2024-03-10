60468a083544f599fa5341f1ccc013a6245c5b16
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2024-03-11 00:34:57 +0300

    Add system test for place menu edit

--- test/system/menus_test.rb
+++ test/system/menus_test.rb
@@ -28,4 +28,28 @@ class MenusTest < ApplicationSystemTestCase
     assert_text "Menu suggestion sent successfully. Thank you for your contribution! 💚"
     assert_equal "Vegan Burger", @place.menus.last.name
   end
+
+  test "editing menu" do
+    user = users(:one)
+    sign_in user
+
+    menu = menus(:one)
+
+    visit place_url(@place)
+
+    click_on "Suggest an Edit"
+
+    find("a[href='/places/#{@place.id}/menus/#{menu.id}/edit']").click
+
+    fill_in "menu_name", with: "New Vegan Burger"
+    fill_in "menu_description", with: "Very Delicious vegan burger"
+    select "Dessert", from: "menu_product_category"
+    fill_in "menu_price", with: "20"
+
+    click_on "Submit"
+    page.driver.browser.switch_to.alert.accept
+
+    assert_text "Menu edit request sent successfully. Thank you for your contribution! 💚"
+    assert_equal "New Vegan Burger", menu.change_logs.last.data["name"]
+  end
 end
