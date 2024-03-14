7c4467ab95a84349b1b39754e52fdf4d2bdd15f5
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2024-03-14 21:37:29 +0300

    Remove cloudfront settings in production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,10 +37,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service_urls_expire_in = 1.week
   config.active_storage.service = :amazon
-  Rails.application.routes.default_url_options[:host] = Rails.application.credentials.dig(:aws, :cloudfront_url)
-
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
