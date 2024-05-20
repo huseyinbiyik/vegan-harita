61aac359bdb42ab8b17510468b47f4bcaba0af02
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2024-05-20 23:34:12 +0300

    Add verify place owner validation

--- app/controllers/place_owner/feedbacks_controller.rb
+++ app/controllers/place_owner/feedbacks_controller.rb
@@ -1,4 +1,6 @@
 class PlaceOwner::FeedbacksController < ApplicationController
+  include PlaceOwner::PlaceOwnerVerification
+
   def show
     @place = Place.find_by(slug: params[:slug])
     @reviews = @place.reviews.order(created_at: :desc).with_attached_images
--- app/controllers/place_owner/qr_code_controller.rb
+++ app/controllers/place_owner/qr_code_controller.rb
@@ -1,5 +1,8 @@
 class PlaceOwner::QrCodeController < ApplicationController
+  include PlaceOwner::PlaceOwnerVerification
+
   before_action :set_place
+
   def show
     @qr = RQRCode::QRCode.new("https://veganharita.com#{place_path(@place.slug)}#menu").as_svg(offset: 0, color: "01da5a", shape_rendering: "crispEdges", module_size: 6)
   end
