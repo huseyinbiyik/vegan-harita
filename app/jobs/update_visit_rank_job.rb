8b7eefcca1d82e8d0d38898db1ada264eec45dcc
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2024-06-02 09:44:44 +0300

    Refactor UpdateVisitRankJob for n+1

--- app/jobs/update_visit_rank_job.rb
+++ app/jobs/update_visit_rank_job.rb
@@ -2,7 +2,12 @@ class UpdateVisitRankJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    sorted_places = Place.all.sort_by { |place| place.visits_count_for_current_month }.reverse
+    visit_counts = Visit.where(created_at: Time.now.beginning_of_month..Time.now.end_of_month)
+                        .group(:place_id)
+                        .count
+
+    sorted_places = Place.all.sort_by { |place| visit_counts[place.id] || 0 }.reverse
+
     sorted_places.each_with_index do |place, index|
       place.update(visit_rank: index + 1)
     end
