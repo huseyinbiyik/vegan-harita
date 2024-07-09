b8c9acfb41a64692893ddfe8fefeb23ea48ac28d
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2024-07-10 00:01:16 +0300

    Remove 'type' column from the 'products' table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_09_204703) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_09_210030) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -162,7 +162,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_09_204703) do
 
   create_table "products", force: :cascade do |t|
     t.string "bar_code"
-    t.integer "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "status", default: false
