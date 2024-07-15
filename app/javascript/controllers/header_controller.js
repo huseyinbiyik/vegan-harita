8ad6c19b716bbc75c32d5b736e51e8edd0aa1d47
Bengi Genç <genc.bengi@gmail.com>
2024-07-15 21:06:48 +0300

    Redisplay header on non-home pages and update element sizes to fix view

---
+++ app/javascript/controllers/header_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  connect() {
+    this.toggleHeader();
+  }
+
+  toggleHeader() {
+    if (window.location.pathname === "/") {
+      this.element.classList.add("passive-header");
+      this.element.classList.remove("active-header");
+    } else {
+      this.element.classList.add("active-header");
+      this.element.classList.remove("passive-header");
+    }
+  }
+}
