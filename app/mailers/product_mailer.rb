127ed54989c167d9d8469ca85c2d20ef9a10174c
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2024-09-16 23:47:13 +0300

    Update  product email template with inline attachments and update logo/image references

--- app/mailers/product_mailer.rb
+++ app/mailers/product_mailer.rb
@@ -3,6 +3,10 @@ class ProductMailer < ApplicationMailer
     @product = params[:product]
     @user = params[:user]
     @url = product_url(@product.slug)
+    attachments.inline["logo.svg"] = Rails.root.join("app", "assets", "images", "logo.svg").read
+    attachments.inline["envelope.svg"] = Rails.root.join("app", "assets", "images", "envelope.svg").read
+    attachments.inline["instagram.svg"] = Rails.root.join("app", "assets", "images", "instagram.svg").read
+
 
     mail(to: @user.email, subject: I18n.t(".new_product_added_subject", contributor: @product.contributors.first.user.username), locale: @user.locale)
   end
