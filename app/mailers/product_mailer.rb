483291d99c0967637978e5b747071a3b41b9d406
Bengi Genç <genc.bengi@gmail.com>
2024-09-17 14:05:49 +0300

    Refactor image attachments in product email template

--- app/mailers/product_mailer.rb
+++ app/mailers/product_mailer.rb
@@ -3,9 +3,9 @@ class ProductMailer < ApplicationMailer
     @product = params[:product]
     @user = params[:user]
     @url = product_url(@product.slug)
-    attachments.inline["logo.svg"] = Rails.root.join("app", "assets", "images", "logo.svg").read
-    attachments.inline["envelope.svg"] = Rails.root.join("app", "assets", "images", "envelope.svg").read
-    attachments.inline["instagram.svg"] = Rails.root.join("app", "assets", "images", "instagram.svg").read
+    attachments.inline["logo.png"] = Rails.root.join("app", "assets", "images", "logo.png").read
+    attachments.inline["envelope.png"] = Rails.root.join("app", "assets", "images", "envelope.png").read
+    attachments.inline["instagram.png"] = Rails.root.join("app", "assets", "images", "instagram.png").read
 
 
     mail(to: @user.email, subject: I18n.t(".new_product_added_subject", contributor: @product.contributors.first.user.username), locale: @user.locale)
