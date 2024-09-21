bd0a22447ddfeb21a62bfbeed428cb023530ba50
Hüseyin Bıyık <tahahuseyinb@gmail.com>
2024-09-21 15:37:57 +0300

    Hotfix for product_sub_category issue

--- app/controllers/product_sub_categories_controller.rb
+++ app/controllers/product_sub_categories_controller.rb
@@ -2,5 +2,6 @@ class ProductSubCategoriesController < ApplicationController
   def index
     product_category = ProductCategory.find(params[:category_id])
     @product_sub_categories = product_category.product_sub_categories
+    @product_sub_category_id = params[:product_sub_category_id]
   end
 end
